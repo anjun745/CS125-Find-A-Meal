67e15bc6a900aff5e8d4d7b0e53cb37726cb9515
Anjun <haj66GY@gmail.com>
2026-01-29 09:54:07 -0800

    calorie equations

--- cs125/src/calories.py
+++ cs125/src/calories.py
@@ -2,3 +2,9 @@ height = 175
 weight = 70
 gender = 1
 age = 20
+
+# calorie predictions:
+if gender == 1:  # male
+    calories = 10 * weight + 6.25 * height - 5 * age + 5
+else:  # female
+    calories = 10 * weight + 6.25 * height - 5 * age - 161
