03ffb4cc95414573861d9ae62b9367a60b2d5d68
Leah Sun <leahs4@uci.edu>
2026-01-31 16:53:05 -0800

    Added search page outline

--- cs125/backend/app.py
+++ cs125/backend/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
 from routes import userinfo_bp
+from routes import search_bp
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(userinfo_bp)
+app.register_blueprint(search_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
