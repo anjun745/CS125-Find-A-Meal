7293d39e04636167d08f446dc6e97f3861e2ab34
Leah Sun <leahs4@uci.edu>
2026-02-05 15:16:30 -0800

    never added the search.js to the repo

---
+++ cs125/src/pages/Search.js
@@ -0,0 +1,56 @@
+import {useState} from "react";
+
+
+export default function Search(){
+    const [query, setQuery] = useState("");
+    const [serverValue, setServerValue] = useState("");
+    const [caloriesFilter, setCaloriesFilter] = useState(false);
+    const [macrosFilter, setMacrosFilter] = useState(false);
+
+    async function search(){
+        const res = await fetch(`http://127.0.0.1:5000/api/search`,{ //?q=${query}`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({query: query, filters: {
+                calories: caloriesFilter,
+                macros: macrosFilter
+            }})
+        });
+        const data = await res.json();
+        setServerValue(data.data); //sends body to server on localhost:5000/api/search
+    }
+
+    return(
+        <div className="search-container">
+            <h2>Search</h2>
+            <p>Enter ingredients, with commas separating each ingredient</p>
+            <input
+                type="text"
+                placeholder="Search by ingredients..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            />
+            <form id="filter-search">
+                <p>Filter Results By</p>
+                <div>
+                    <label>
+                        <input type="checkbox" name="calories" value="calories"
+                        checked={caloriesFilter}
+                        onChange={(e) => setCaloriesFilter(e.target.checked)}
+                        />
+                        Recommended Calories
+                    </label>
+                </div>
+                <label>
+                    <input type="checkbox" name="macros" value="macros"
+                    checked={macrosFilter}
+                    onChange={(e) => setMacrosFilter(e.target.checked)}
+                    />
+                    Macro Restrictions
+                </label>
+            </form>
+            <button onClick={search}>Search</button>
+            <p>Server Response: {JSON.stringify(serverValue, null, 2)}</p>
+        </div>
+    );
+}
